764a9f3406af49ad40d62c20a40b16d4b4843887
Алексей Папков <steam07151@gmail.com>
2022-11-23 20:08:30 +0300

    Update name

--- src/main/kotlin/com/itmo/microservices/demo/agregate/UserCartState.kt
+++ src/main/kotlin/com/itmo/microservices/demo/agregate/UserCartState.kt
@@ -1,6 +1,5 @@
 package com.itmo.microservices.demo.agregate
 
-import com.itmo.microservices.demo.tasks.api.messaging.TaskCreatedEvent
 import ru.quipy.core.annotations.DomainEvent
 import ru.quipy.core.annotations.StateTransitionFunc
 import ru.quipy.domain.AggregateState
@@ -9,10 +8,9 @@ import java.util.*
 
 const val TAG_CREATED_CHECKOUT = "CREATE_CHECKOUT"
 const val TAG_ADD_PAPPER = "ADD_PAPPER"
-const val BANK_ACCOUNT_WITHDRAWAL = "BANK_ACCOUNT_WITHDRAWAL_EVENT"
-const val INTERNAL_ACCOUNT_TRANSFER = "INTERNAL_ACCOUNT_TRANSFER_EVENT"
 
-class UserCheckoutState: AggregateState<String, UserCheckout> {
+
+class UserCartState: AggregateState<String, UserCart> {
     private lateinit var trackId: String
     private var createdAt: Long = System.currentTimeMillis()
     private var updatedAt: Long = System.currentTimeMillis()
@@ -40,7 +38,7 @@ data class PappersEntity(
 @DomainEvent(name = TAG_CREATED_CHECKOUT)
 class TagCreatedCheckoutEvent(
     createdAt: Long = System.currentTimeMillis(),
-) : Event<UserCheckout>(
+) : Event<UserCart>(
     name = TAG_CREATED_CHECKOUT,
     createdAt = createdAt,
 )
@@ -49,16 +47,16 @@ class TagCreatedCheckoutEvent(
 class TagAddPaper(
     val papperId: UUID,
     val namePapper: String
-) : Event<UserCheckout>(
+) : Event<UserCart>(
     name = TAG_ADD_PAPPER,
 )
 
-fun UserCheckout.createUserCheckout(
+fun UserCart.createUserCheckout(
 ): TagCreatedCheckoutEvent {
     return TagCreatedCheckoutEvent()
 }
 
-fun UserCheckout.createAddPapper(
+fun UserCart.createAddPapper(
     papperId: UUID,
     namePapper: String
 ): TagAddPaper {
