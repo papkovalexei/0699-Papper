a4278362c4f780a713eeaec1448e324af1f0a75e
Алексей Папков <steam07151@gmail.com>
2022-11-23 21:03:27 +0300

    Impl service

---
+++ src/main/kotlin/com/itmo/microservices/demo/cart/impl/service/CartControllerImpl.kt
@@ -0,0 +1,17 @@
+package com.itmo.microservices.demo.cart.impl.service
+
+import com.itmo.microservices.demo.checkout.api.service.CartService
+import org.springframework.stereotype.Service
+import java.util.*
+
+@Service
+class CartControllerImpl(
+
+) : CartService {
+    override fun addPapper(id: UUID) {
+
+    }
+    override fun removePaper(id: UUID) {
+
+    }
+}
